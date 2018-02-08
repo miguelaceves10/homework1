86f8eaf41b3e35a02ada3e185a9ae7df3b4dd17c
Miguel A Aceves <maceves@csumb.edu>
2018-02-08 07:19:04 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title> Programming Languages </title>
+        
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        
+    
+        <header>
+            <h1>Programming Languages</h1>
+        </header>
+        <nav>
+            <hr width="50%"/>
+           <ul id= "menu">
+            
+            <li><a href="index.php">Home</a></li>
+            <li><a href="python.html">Python</a></li>
+            <li><a href="ruby.html">Ruby</a></li>
+            <li><a href="swift.html">Swift</a></li>
+            </u>
+        </nav>
+        
+        <br /> <br />
+        
+        <main>
+            <figure id="cspic">
+                <img width="350" height="300" src="img/cs1.jpg" />
+            </figure>
+            
+            <div id="main">
+                <strong>What is a Programming language?</strong> <br />
+                <p>Computer programming languages allow us to give instructions to a computer in a language the computer understands. Just as many human-based languages exist, there are an array of computer programming languages that programmers can use to communicate with a computer.</p>
+                <p>The portion of the language that a computer can understand is called a “binary.”Translating programming language into binary is known as “compiling.” Each language, like Python, Ruby, and Swift, has its own distinct features, though many times there are commonalities between programming languages.</p>
+                <p>Learn a new language today!</p>
+                
+                
+                
+            </div>
+            
+        </main>
+        
+        <footer>
+            <hr>
+            Internet Programming CST 336. 2017&copy;Miguel Aceves <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous.<br />
+            It is used for academic purposes only.
+            
+            <figure>
+                 <img width="100" height="100" src="img/csumblogo.jpeg" />
+            </figure>
+            <a href="https://www.computerscience.org/resources/computer-programming-languages/">Computer Science.org</a>
+        </footer>
+        
+       
+        
+    </body>
+ 
+
+</html>
